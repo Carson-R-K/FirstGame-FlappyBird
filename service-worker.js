1592cf5de4b9c9b72b26cbe0dec0f4dcd8997573
Carson-R-K <carsonkennington@gmail.com>
2025-12-30 16:12:55 -0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('flappy-bird-cache').then(cache => {
+      return cache.addAll([
+        'index.html',
+        'style.css',
+        'game.js',
+        'manifest.json',
+        'icon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
